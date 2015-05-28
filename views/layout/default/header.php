48dd83fbedd5f1d7e95269b8135d479103f15130
lgaleanoc <lgaleanoc@BOGODCDIG7G5571.SENA.RED>
2015-05-28 14:40:22 -0500

    Merge origin/master
    Conflicts:
    	modules/administrador/views/permisos/permisos.phtml

--- views/layout/default/header.php
+++ views/layout/default/header.php
@@ -27,76 +27,76 @@
                 <img src="<?php echo BASE_URL . 'public/img/header.jpg'; ?>" class="img-thumbnail" width="100%">
             </div>
             <nav class="navbar navbar-default">
-                <a class="navbar-brand"><?php echo APP_NAME; ?></a>
+                <a class="navbar-brand" href=""><?php echo APP_NAME; ?></a>
                 <ul id="menuPrincipal" class="ulMenuPrincipal nav nav-tabs">
                     <?php if (isset($_layoutParams['menu'])): ?>
                         <?php for ($i = 0; $i < count($_layoutParams['menu']); $i++): ?>
                             <?php
                             if ($item && $_layoutParams['menu'][$i]['id'] == $item)
                             {
-                                $_item_style = 'active';
+                                $_item_style = 'current';
                             }
                             else
                             {
                                 $_item_style = '';
                             }
                             ?>        
-                            <li class="liPrin liPrin-<?php echo $i . ' ' . $_item_style; ?>">
-                                <a href="<?php echo $_layoutParams['menu'][$i]['enlace']; ?>">
+                            <li class="liPrin liPrin-<?php echo $i; ?> <?php echo ($item && $_layoutParams['menu'][$i]['id'] == $item) ? 'active' : '' ?> <?php echo (isset($_layoutParams['menu'][$i]['submenu']) && $_layoutParams['menu'][$i]['submenu']) ? "dropdown" : "" ?>">
+                                <a class="<?php echo $_item_style; ?> dropdown-toggle" <?php echo ($item && $_layoutParams['menu'][$i]['id'] == $item) ? "data-toggle='dropdown'" : '' ?> href="<?php echo isset($_layoutParams['menu'][$i]['enlace']) ? $_layoutParams['menu'][$i]['enlace'] : ""; ?>">
                                     <?php echo $_layoutParams['menu'][$i]['titulo']; ?>
                                 </a>	
-                                <?php if (isset($_layoutParams['menu'][$i]['submenu']) && $_layoutParams['menu'][$i]['submenu']): ?>
-                                    <ul class="ulSubMenu oculto">
+                                    <?php if (isset($_layoutParams['menu'][$i]['submenu']) && $_layoutParams['menu'][$i]['submenu']): ?>
+                                    <ul class="ulSubMenu oculto dropdown-menu">
                                         <?php for ($j = 0; $j < count($_layoutParams['menu'][$i]['subArray']); $j++): ?>
                                             <?php
                                             if ($item && $_layoutParams['menu'][$i]['subArray'][$j]['id'] == $item)
                                             {
-                                                $_item_style = 'active';
+                                                $_item_style = 'current';
                                             }
                                             else
                                             {
                                                 $_item_style = '';
                                             }
                                             ?>        
-                                            <li class="liSubMenu liSubMenu-<?php echo $j . ' ' . $_item_style; ?>">
-                                                <a href="<?php echo $_layoutParams['menu'][$i]['subArray'][$j]['enlace']; ?>">
+                                            <li class="liSubMenu liSubMenu-<?php echo $j; ?>">
+                                                <a class="<?php echo $_item_style; ?> textCapital" href="<?php echo $_layoutParams['menu'][$i]['subArray'][$j]['enlace']; ?>">
                                                     <?php echo $_layoutParams['menu'][$i]['subArray'][$j]['titulo']; ?>
-                                                </a>
-                                                <?php if (isset($_layoutParams['menu'][$i]['subArray'][$j]['submenu']) && $_layoutParams['menu'][$i]['subArray'][$j]['submenu']): ?>
+                                                </a>	
+                                                    <?php if (isset($_layoutParams['menu'][$i]['subArray'][$j]['submenu']) && $_layoutParams['menu'][$i]['subArray'][$j]['submenu']): ?>
                                                     <ul class="ulSubMenu2 ulSubMenu2-<?php echo $j; ?> oculto">
                                                         <?php for ($k = 0; $k < count($_layoutParams['menu'][$i]['subArray'][$j]['subArray']); $k++): ?>
                                                             <?php
                                                             if ($item && $_layoutParams['menu'][$i]['subArray'][$j]['subArray'][$k]['id'] == $item)
                                                             {
-                                                                $_item_style = 'active';
+                                                                $_item_style = 'current';
                                                             }
                                                             else
                                                             {
                                                                 $_item_style = '';
                                                             }
                                                             ?>        
-                                                            <li class="liSubMenu2-<?php echo $k . ' ' . $_item_style; ?>">
-                                                                <a href="<?php echo $_layoutParams['menu'][$i]['subArray'][$j]['subArray'][$k]['enlace']; ?>">
-                                                                    <?php echo $_layoutParams['menu'][$i]['subArray'][$j]['subArray'][$k]['titulo']; ?>
+                                                            <li class="liSubMenu2-<?php echo $k; ?>">
+                                                                <a class="<?php echo $_item_style; ?> textCapital" href="<?php echo $_layoutParams['menu'][$i]['subArray'][$j]['subArray'][$k]['enlace']; ?>">
+                                                <?php echo $_layoutParams['menu'][$i]['subArray'][$j]['subArray'][$k]['titulo']; ?>
                                                                 </a>
                                                             </li>
-                                                        <?php endfor; ?>
-                                                    </ul>
-                                                <?php endif; ?>
-                                            </li>
                                         <?php endfor; ?>
+                                                    </ul>
+                <?php endif; ?>
+                                            </li>
+                            <?php endfor; ?>
                                     </ul>
-                                <?php endif; ?>
+                        <?php endif; ?>
                             </li>
-                        <?php endfor; ?>
-                    <?php endif; ?>
+                    <?php endfor; ?>
+                <?php endif; ?>
                 </ul>
             </nav>
             <div id="content" class="container-fluid contenido">
-                <?php if (isset($this->_error)): ?>
+<?php if (isset($this->_error)): ?>
                     <div id="error"><?php echo $this->_error; ?></div>
-                <?php endif; ?>
+<?php endif; ?>
 
-                <?php if (isset($this->_mensaje)): ?>
+<?php if (isset($this->_mensaje)): ?>
                     <div id="mensaje"><?php echo $this->_mensaje; ?></div>
-                <?php endif; ?>
+<?php endif; ?>
